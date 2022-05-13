d5b2eff124a7ae43b1bbe9b945f3896e69653789
mike_karni <3923499@mail.ru>
2022-05-13 18:38:45 +0300

    FiRstTaskDOne

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте двумерный массив размером mxn, заполненный случайными вещественными числами
+Console.Write("Введите m: "); // Демонстрационная задача
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+
+
+for (int i = 0; i < m; i++)
+{
+     
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().NextDouble()*(10-1)+1;
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
