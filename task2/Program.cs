fea76bb929cac06a42e8b69f487af127e659a6bd
mike_karni <3923499@mail.ru>
2022-05-14 18:48:06 +0300

    SecondTaskDone

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,33 +1,17 @@
 ﻿/*Напишите программу, которая на вход принимает позиции элемента в двумерном массиве и 
     возвращает значение этого элемента или же указание, что такого элемента нет.*/
+
 Console.Write("Введите количество строк m: "); 
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов n: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите позицию в строке искомого элемента : ");
+Console.Write("Введите индекс  искомого элемента в строке : ");
 int positionString = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите позицию в столбце искомого элемента : ");
+Console.Write("Введите индекс  искомого элемента в столбце : ");
 int positionColumn = Convert.ToInt32(Console.ReadLine());
-
 int[,] array = new int[m,n];
-
-for (int i = 0; i < m; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        array[i, j] = new Random().Next(1, 11);
-    }
-}
-
-for (int i = 0; i < m; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        Console.Write(array[i, j] + " ");
-    }
-    Console.WriteLine();
-}
-
+FillArray(array);
+PrintArray(array);
 
 
 for (int i = 0; i < m; i++)
@@ -36,22 +20,40 @@ for (int i = 0; i < m; i++)
     {
         if (positionColumn >= 0 && positionString >= 0 && positionString == i && positionColumn == j)
         {
-            Console.WriteLine($"Искомый элемент равен = {array[positionString,positionColumn]} ");
+            Console.WriteLine($"Искомый элемент равен  {array[positionString,positionColumn]}");
             break;
-        }
-        
-        else if (positionColumn >= 0 && positionString >= 0 && positionString > i || positionColumn > j)
-        {
-            Console.WriteLine($" элемента со строкой {positionString} и столбцом  {positionColumn} в массиве нет");
-            break;
-            
-            
-        }
-        
+        }    
     }
- 
+ if (positionColumn >= 0 && positionString >= 0 && positionString >= m || positionColumn >= n)
+ {
+     Console.WriteLine($" элемента с индексом {positionString} строки и индексом  {positionColumn} столбца в массиве нет");
+            break;
+ }
  Console.WriteLine();
 }
-//Console.WriteLine($" элемента {positionString}  {positionColumn} в массиве нет");
 
 
+//МЕТОД ПЕЧАТИ МАССИВА
+void PrintArray(int[,] matr) 
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            Console.Write($" {matr[rows, columns]} ");   
+        }
+        Console.WriteLine();
+    }
+}
+// МЕТОД ЗАПОЛНЕНИЯ МАССИВА
+void FillArray(int[,] matr) 
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows,columns] = new Random().Next(1,11); 
+        }
+        Console.WriteLine();
+    }
+}
